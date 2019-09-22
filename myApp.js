45a61e4f1c808e0f671cac9ca1b982509a8ceec4
Glitch (sundhar811-basic-node-and-express) <none>
2019-09-22 06:27:34 +0000

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -25,15 +25,22 @@ app.get('/', (req, res) => {
 app.use(express.static(__dirname + '/public'))
 
 /** 5) serve JSON on a specific route */
-app.get('/json', (req, res) => {
-  res.json({
-    'message': 'Hello json'
-  });
-})
+// app.get('/json', (req, res) => {
+//   res.json({
+//     'message': 'Hello json'
+//   });
+// })
 
 /** 6) Use the .env file to configure the app */
- 
- 
+let message = 'Hello json'
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ message: message.toUpperCase() });
+  } else {
+    res.json({ message: message })
+  }
+})
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
